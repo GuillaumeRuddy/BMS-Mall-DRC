b9b28cbfeac1ed1a77f339af2cd5d3dd6f05bf82
Profondeur <ProfondeurAgency@gmail.com>
2022-11-28 22:37:07 +0100

    gestion de la partie adresse rapide2

--- lib/pages/coordoner.dart
+++ lib/pages/coordoner.dart
@@ -24,82 +24,126 @@ class _CoordonneState extends State<Coordonne> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: ListView(children: [
-        Container(
-          color: AppColors.blueR,
-          padding: EdgeInsets.all(15),
-          //entete
-          child: Row(
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                  size: 25.0,
-                ),
+    return Scaffold(
+        body: Form(
+          key: formKey,
+          child: ListView(children: [
+            Container(
+              color: AppColors.blueR,
+              padding: EdgeInsets.all(15),
+              //entete
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 25.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "Carte",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: Text(
-                  "Carte",
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 50,
+                          ),
+                          nomChampSaisie(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextButton(
+                              onPressed: () async {
+                                LatLng? reponse = await Navigator.push<LatLng>(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (ctx) => MapsPage()));
+                                ;
+                                if (reponse != null) {
+                                  long = "${reponse.longitude}";
+                                  lat = "${reponse.latitude}";
+                                  print(reponse.latitude);
+                                  print(reponse.longitude);
+                                }
+                              },
+                              child: Text('Aller vers la Carte')),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          descriptionChampSaisie(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  //    Spacer(),
+                ],
+              ),
+            ),
+          ]),
+        ),
+        bottomNavigationBar: //Validation et montant total
+            BottomAppBar(
+                child: Container(
+          height: 70,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: AppColors.blueR,
+              borderRadius: BorderRadius.circular(1),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                    offset: Offset(0, 3))
+              ]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(CupertinoIcons.cart_badge_plus),
+                label: Text(
+                  "Valider",
                   style: TextStyle(
-                      fontSize: 20.0,
+                      fontSize: 16,
                       fontWeight: FontWeight.bold,
                       color: Colors.white),
                 ),
-              ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(AppColors.ecrit),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 13, horizontal: 15),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)))),
+              )
             ],
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 50,
-                      ),
-                      nomChampSaisie(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextButton(
-                          onPressed: () async {
-                            LatLng? reponse = await Navigator.push<LatLng>(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (ctx) => MapsPage()));
-                            ;
-                            if (reponse != null) {
-                              long = "${reponse.longitude}";
-                              lat = "${reponse.latitude}";
-                              print(reponse.latitude);
-                              print(reponse.longitude);
-                            }
-                          },
-                          child: Text('Aller vers la Carte')),
-                      SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              //    Spacer(),
-              boutonEnvoyer(context)
-            ],
-          ),
-        ),
-      ]),
-    );
+        )));
   }
 
   Widget nomChampSaisie() {
@@ -116,7 +160,7 @@ class _CoordonneState extends State<Coordonne> {
             Icons.timer,
             color: Colors.grey,
           ),
-          labelText: "Prix unitaire",
+          labelText: "Nom",
           labelStyle:
               TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
           border: UnderlineInputBorder(
@@ -138,7 +182,7 @@ class _CoordonneState extends State<Coordonne> {
 
   Widget descriptionChampSaisie() {
     return TextFormField(
-      controller: nomCtrl,
+      controller: detailCtrl,
       readOnly: true,
       keyboardType: TextInputType.number,
       validator: (String? value) {
@@ -150,7 +194,7 @@ class _CoordonneState extends State<Coordonne> {
             Icons.timer,
             color: Colors.grey,
           ),
-          labelText: "Prix unitaire",
+          labelText: "Détails",
           labelStyle:
               TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
           border: UnderlineInputBorder(
